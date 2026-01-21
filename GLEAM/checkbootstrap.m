2c241189a9f2bcf3a56115d2b3e7810ca67ee312
Takumi Abekawa <135727905+tabekawa@users.noreply.github.com>
2026-01-21 19:08:52 +0900

    Delete GLEAM directory

--- GLEAM/checkbootstrap.m
+++
@@ -1,9 +0,0 @@
-function notopt = checkbootstrap(Z)
-    notopt = [];
-        for j = 1:lenght(Z)
-            if Z(j) >= 10
-            notopt = [notopt;j];
-            disp(Z(j))
-            end
-        end
-end
